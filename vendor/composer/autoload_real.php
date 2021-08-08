9f8e7b8e076ddc4d107330be88e9f8e2defbead9
Jordan Pakrosnis <jpak@jordanpak.com>
2021-08-07 22:47:05 -0400

    Rebuild autoload for real use

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,24 +50,6 @@ class ComposerAutoloaderInitad1b5919ccfd20253dd308dd06ff5d51
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitad1b5919ccfd20253dd308dd06ff5d51::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequiread1b5919ccfd20253dd308dd06ff5d51($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequiread1b5919ccfd20253dd308dd06ff5d51($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
